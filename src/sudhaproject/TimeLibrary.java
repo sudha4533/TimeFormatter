3d5765d3c4e2c80303a2308d40d186949c12113c
Sudha <Sudha@10.21.26.250>
2015-10-20 20:35:55 -0400

    First Commit

---
+++ src/sudhaproject/TimeLibrary.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sudhaproject;
+import java.util.*;
+/**
+ *
+ * @author Sudha
+ */
+public class TimeLibrary {
+    public static String stringForDate(Date date){
+        Date currentdate = new Date();
+        
+    return "";
+    }
+    public static String stringForTimeInterval(int se){
+    
+    return "";
+    }
+
+    
+}
